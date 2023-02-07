6f648e82e7a70ca036c33b3c119431fb5cb7e213
okera <okerajohnson@gmail.com>
2023-02-07 16:40:51 -0500

    tweek

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 
 
 
-const welcome = {
-title: 'React',
-greeting: 'Heelo'
-};
+function getTitle(title) {
+  return title + ' please';
+}
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>Hello {getTitle('React')}</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
